734797e33dc1aa85a1f1cb41bd5303a4d9e701d6
chliyang <chengli.yang@thoughtworks.com>
2022-03-30 23:47:22 +0800

    learn to use antd to create react app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout, Menu, Avatar } from "antd";
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  SnippetsOutlined,
+  RadarChartOutlined,
+} from "@ant-design/icons";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-     react-antd
-    </div>
-  );
+const { Header, Sider, Content } = Layout;
+
+class App extends React.Component {
+  state = {
+    collapsed: false,
+  };
+
+  toggle = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
+  render() {
+    return (
+      <Layout className="h-full">
+        <Sider
+          className="side"
+          trigger={null}
+          collapsible
+          collapsed={this.state.collapsed}
+        >
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              个人基本信息
+            </Menu.Item>
+            <Menu.Item key="3" icon={<RadarChartOutlined />}>
+              个人技能
+            </Menu.Item>
+            <Menu.Item key="2" icon={<SnippetsOutlined />}>
+              备忘录
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(
+              this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined,
+              {
+                className: "trigger",
+                onClick: this.toggle,
+              }
+            )}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: "24px 16px",
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <Avatar
+              size={64}
+              style={{
+                backgroundColor: "#1890ff",
+              }}
+              icon={<UserOutlined />}
+            />
+            <span style={{ fontSize: "20px", marginLeft: "20px" }}>
+              设计能力有限
+            </span>
+            <p style={{ marginTop: "60px" }}>后续内容，敬请期待。。。。。。</p>
+          </Content>
+        </Layout>
+      </Layout>
+    );
+  }
 }
 
 export default App;
